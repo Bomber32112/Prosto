87eb5f01a1bae9276e4faa2a4499bd803b83cee7
Gotovo <BETEP2100@mail.ru>
2024-09-19 13:13:05 +1000

    прр

--- ConsoleApp1125НИ/Program.cs
+++ ConsoleApp1125НИ/Program.cs
@@ -4,36 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Номер главы");
-            int.TryParse(Console.ReadLine(), out int number_of_head);
-            Console.WriteLine("Номер задания");
-            int.TryParse(Console.ReadLine(), out int number_of_execise);
-            switch (number_of_head)
-            {
-                case 1:
-                    switch (number_of_execise)
-                        {
-                        case 1: N1_1();
-                            break;
-                        case 6: N1_6(); break;
+            var program = new Program();
+            var method = program.GetType().GetMethod(Console.ReadLine());
+            method.Invoke(program, null);
+            //Console.WriteLine("Номер главы");
+            //int.TryParse(Console.ReadLine(), out int number_of_head);
+            //Console.WriteLine("Номер задания");
+            //int.TryParse(Console.ReadLine(), out int number_of_execise);
+            //switch (number_of_head)
+            //{
+            //    case 1:
+            //        switch (number_of_execise)
+            //            {
+            //            case 1: N1_1();
+            //                break;
+            //            case 6: N1_6(); break;
                         
-                        case 16: N1_16(); break;
-                    }
-                break;
-                case 2:
-                    switch (number_of_execise)  
-                    {
-                        case 1: N2_1(); break;
-                            case 6: N2_6(); break;
-                        case 8: N2_8(); break;
-                        case 9: N2_10(); break;
-                        case 36: N2_36(); break;
-                    }
-                    break;
+            //            case 16: N1_16(); break;
+            //        }
+            //    break;
+            //    case 2:
+            //        switch (number_of_execise)  
+            //        {
+            //            case 1: N2_1(); break;
+            //                case 6: N2_6(); break;
+            //            case 8: N2_8(); break;
+            //            case 9: N2_10(); break;
+            //            case 36: N2_36(); break;
+            //        }
+            //        break;
 
-            }
+            //}
         }
-        static void N1_1 () 
+        public static void N1_1 () 
         {
             int.TryParse(Console.ReadLine(), out int number_1);
             int.TryParse(Console.ReadLine(), out int number_2);
@@ -119,6 +122,95 @@
         {
             
             
+        }
+        public static void N4_118() 
+        {
+            int.TryParse (Console.ReadLine(), out int x);
+            if (x <= 0) x = 0;
+            else if (x > 0 && x <= 1);
+            else x *= x;
+            Console.WriteLine($"f(x) = {x}");
+        }
+        public static void N4_119() 
+        {
+            int.TryParse(Console.ReadLine(), out int y);
+            if (y > 2) y = 2;
+            else if (y > 0 && y <= 2);
+            else y = (~y + 1)*3;
+            Console.WriteLine($"f(y) = {y}");
+        }
+        public static void N4_120()
+        {
+            int.TryParse(Console.ReadLine(), out int under);
+            int.TryParse(Console.ReadLine(), out int x);
+            double y;
+            switch (under)
+            {
+                case 1:
+            
+            
+            if (x >= -1 && x <= 1) y = 1; break;
+            case 2:
+                    if (x >= -1 && x<= 1) y = ~x+1;
+             break;
+            case 3:
+                    if (x >= -1 && x <= 1) y = x - 0.5;
+                    break;
+            }
+        }
+        public static void N4_133() 
+        {
+            int.TryParse(Console.ReadLine(), out int x);
+            switch (x)
+            {
+                case 1: Console.WriteLine("Понедельник"); break;
+                case 2: Console.WriteLine("Вторник"); break;
+                case 3: Console.WriteLine("Среда"); break;
+                case 4: Console.WriteLine("Четверг"); break;
+                case 5: Console.WriteLine("Пятница"); break;
+                case 6: Console.WriteLine("Суббота"); break;
+                case 7: Console.WriteLine("Воскресенье"); break;
+            }
+        }
+        public static void N4_135() 
+        {
+            int.TryParse(Console.ReadLine(), out int x);
+            Console.Write(test(x));
+            static string test(int x) => x 
+            switch
+            {
+                1 or 2 or 12 => "Зима",
+                >=3 and <=5 => "Весна",
+                >= 6 and <= 8 => "Лето",
+                >= 9 and <= 11 => "Осень"
+            };
+        }
+        public static void N4_136() 
+        {
+            int.TryParse(Console.ReadLine(), out int month);
+            bool.TryParse(Console.ReadLine(), out bool x);
+            if (month == 2 && x) Console.WriteLine(29); else Console.WriteLine(test(month));
+            static int test(int month) => month
+            switch
+            {
+                1 or 3 or 5 or 7 or 8 or 10 or 12 => 31,
+                2 => 28,
+                4 or 6 or 9 or 11 => 30
+            };
+        }
+        public static void N4_138() 
+        {
+            int.TryParse(Console.ReadLine(), out int number);
+            switch (number) 
+            {
+                case 1: Console.WriteLine("Пики"); break;
+                case 2:
+                    Console.WriteLine("Трефы"); break;
+                case 3:
+                    Console.WriteLine("Бубны"); break;
+                case 4:
+                    Console.WriteLine("Черви"); break;
+            }
         }
     }
     static public class Term 
